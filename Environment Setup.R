9c3fbb0c3da3c546daa854b6279f7dd33f4ad268
Matthew Hendrickson <mjhendrickson1@gmail.com>
2018-03-07 22:11:29 -0500

    Needed packages / libraries
    Consolidated previous files and includes a list of needed packages/libraries.

---
+++ Environment Setup.R
@@ -0,0 +1,18 @@
+# ===== Introduction =====
+# Project: Learning R by Doing - HarvardX -MITx Analysis
+# By: Matthew Hendrickson
+# What: Environment Setup
+
+# ===== Install needed packages =====
+install.packages("readr")
+install.packages("tidyverse")
+install.packages("DataExplorer")
+
+# ===== Load needed packages =====
+library(readr)
+library(tidyverse)
+library(ggplot2)
+library(scales)
+library(dplyr)
+library(stringr)
+library(DataExplorer)
