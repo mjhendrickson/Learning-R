521921f5a6149c5726de878e43e016e50664c071
Matthew Hendrickson <mjhendrickson1@gmail.com>
2018-03-23 22:14:56 -0400

    removed tidyverse redundancies
    readr, dplyr, stringr, ggplot2 were redundant with tidyverse install/load

--- 1.Environment Setup.R
+++ 1.Environment Setup.R
@@ -4,17 +4,12 @@
 # What: Environment Setup
 
 # ===== Install needed packages =====
-install.packages("readr")
 install.packages("tidyverse")
 install.packages("binr")
 install.packages("DataExplorer")
 
 # ===== Load needed packages =====
 library(tidyverse)
-library(readr)
-library(dplyr)
-library(stringr)
-library(ggplot2)
 library(scales)
 library(lubridate)
 library(binr)
